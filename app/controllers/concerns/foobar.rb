5bff38ea77cc27049ab63b7a310bf99a67dd0eaa
Kai-Li Yen <kailiyen@berkeley.edu>
2016-09-22 21:09:17 -0700

    completed hw2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,14 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+
+  attr_accessor :my_word
+
   def initialize(param)
+  	@my_word = param
   end
+
+  def bar(a, b)
+  	a.to_s + @my_word + b[:sat].to_s
+  end
+
 end
--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -8,21 +8,29 @@ class Person
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'nickname'
+    @nickname = @name[0..3]
+
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    2016 - @age.to_i
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'introduce'
+    @name + @age
   end
 
   def fib_number
     # YOUR IMPLMENTATION HERE
-    0
+    fib_helper(@age.to_i)
+  end
+
+  def fib_helper(n)
+    if n <= 1
+      return n
+    end
+    fib_helper(n - 1) + fib_helper(n - 2)
   end
 end
